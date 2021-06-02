ff566bc7a0b77be3296b842ff611a92ffa1732c9
Toti Cinque <director@emoney.tech>
2021-06-02 11:01:59 +0200

    update workers

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,8 +8,10 @@ sidekiq_config = { url: ENV['JOB_WORKER_URL'] }
 Sidekiq.configure_server do |config|
   config.redis = sidekiq_config
   config.failures_max_count = 5000
+  Sidekiq::Status.configure_server_middleware config, expiration: 30.minutes
 end
 
 Sidekiq.configure_client do |config|
   config.redis = sidekiq_config
+  Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
 end
