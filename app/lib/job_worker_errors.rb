fe18d54c56ab8cb6d1101bc9b5a87d0261511d65
Toti Cinque <director@emoney.tech>
2021-06-02 10:46:19 +0200

    update workers

--- app/lib/job_worker_errors.rb
+++
@@ -1,8 +0,0 @@
-class JobWorkerErrors < StandardError
-  attr_reader :opt
-
-  def initialize(msg="Default msg Job Worker Error", error="default error", opt="default opt")
-    WorkerError.create(name: msg, error: error, params: param)
-    super(msg)
-  end
-end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,9 +6,6 @@ sidekiq_config = { url: ENV['JOB_WORKER_URL'] }
 Sidekiq.configure_server do |config|
   config.redis = sidekiq_config
   config.failures_max_count = 5000
-  config.death_handlers << ->(job, ex) do
-    JobWorkerErrors.new("#{job['class']} #{job["jid"]}", "#{ex.message}", "#{job["args"]}")
-  end
 end
 
 Sidekiq.configure_client do |config|
