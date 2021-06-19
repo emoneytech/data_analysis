806f9d1d7dded0678464cd94268e028fbaa2c9c4
Toti Cinque <director@emoney.tech>
2021-06-19 10:15:06 +0200

    bugfix

--- app/workers/create_evaluated_risk_worker.rb
+++ app/workers/create_evaluated_risk_worker.rb
@@ -2,7 +2,7 @@ class CreateEvaluatedRiskWorker
   include Sidekiq::Worker
   include Sidekiq::Status::Worker
   # sidekiq_options queue: 'critical', retry: false, backtrace: true
-  sidekiq_options queue: 'critical', retry: false, backtrace: true
+  sidekiq_options queue: 'default', retry: false, backtrace: true
   
   # PARAMS
   # customer_id, day, default_risk, divisor_amount_for_factor, factor_for_amount, max_base_risk
