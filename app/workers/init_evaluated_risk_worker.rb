a605f5c42438ef2a45f362552e666d389f597f82
Toti Cinque <director@emoney.tech>
2021-06-21 11:16:56 +0200

    bugfix

--- app/workers/init_evaluated_risk_worker.rb
+++ app/workers/init_evaluated_risk_worker.rb
@@ -8,6 +8,11 @@ class InitEvaluatedRiskWorker
   # customer_id, day, default_risk, divisor_amount_for_factor, factor_for_amount, max_base_risk
 
   def perform()
+    default_risk = Configurable.min_base_risk.to_f
+    divisor_amount_for_factor = Configurable.divisor_amount_for_factor.to_f
+    factor_for_amount = Configurable.factor_for_amount.to_f
+    max_base_risk = Configurable.max_base_risk.to_f
+
     Anagrafica.alive.order(Created: :asc).select(:IdUtente).find_in_batches(batch_size: 500) do |customers|
       customers.each do |customer|
         CreateEvaluatedRiskWorker.perform_async(customer.IdUtente,default_risk,divisor_amount_for_factor,factor_for_amount,max_base_risk)
