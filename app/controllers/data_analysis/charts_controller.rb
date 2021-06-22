e823c0d28d3a5b2419bcd41d1d957bebc6155267
Toti Cinque <director@emoney.tech>
2021-06-22 12:00:22 +0200

    bugfix

--- app/controllers/data_analysis/charts_controller.rb
+++ app/controllers/data_analysis/charts_controller.rb
@@ -4,7 +4,7 @@ module DataAnalysis
     before_action(only: :index) { authorize! :admin, :dashboard }
     
     def all_risk_movements
-      render json: RiskMovement.where("movement_amount > ?", "1".to_f).by_year.group(:table_name).group_by_month(:movement_created_at).count
+      render json: RiskMovement.where("movement_amount > ?", "1".to_f).by_year.group(:table_name).group_by_month(:movement_created_at).count.chart_json
     end
 
   end
