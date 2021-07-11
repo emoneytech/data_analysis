5b0af4eed06eb5cee19077154044c83e9a72078d
Toti Cinque <director@emoney.tech>
2021-07-11 10:49:38 +0200

    add markercluster

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -10,7 +10,7 @@ Geocoder.configure(
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
   # api_key: nil,               # API key for geocoding service
-  cache: Redis.new(url: ENV['JOB_WORKER_URL']),   # cache object (must respond to #[], #[]=, and #del)
+  cache: Redis.new(url: ENV['GECODER_REDIS_CACHE']),   # cache object (must respond to #[], #[]=, and #del)
   cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
   # Exceptions that should not be rescued by default
