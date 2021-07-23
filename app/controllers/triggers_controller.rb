5cdc5152efba7fd0f9cc47ad1bff31ffa1832582
Toti Cinque <director@emoney.tech>
2021-07-23 12:14:56 +0200

    bugfix

--- app/controllers/triggers_controller.rb
+++ app/controllers/triggers_controller.rb
@@ -2,7 +2,7 @@ class TriggersController < ApplicationController
 
   def services
     service = Servizio.find(params[:id])
-    CreateEvalMovementWorker(service.idservizio, service.point, Configurable.default_product_base_risk.to_f)
+    CreateEvalMovementWorker.perform_async(service.idservizio, service.point, Configurable.default_product_base_risk.to_f)
     head :ok
   end
 
