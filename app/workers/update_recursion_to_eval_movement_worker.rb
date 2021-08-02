87d10ba379c7044d49e61d5a07f83dbc57b2617a
Toti Cinque <director@emoney.tech>
2021-08-02 11:20:45 +0200

    bugfix

--- app/workers/update_recursion_to_eval_movement_worker.rb
+++ app/workers/update_recursion_to_eval_movement_worker.rb
@@ -6,7 +6,7 @@ class UpdateRecursionToEvalMovementWorker
 
   def perform(eval_movement_id)
     em = EvalMovement.find eval_movement_id
-    em.update(
+    em.update_columns(
       recursion_all_7:       em.recursion['all']['day_7'].to_i,
       recursion_all_30:      em.recursion['all']['day_30'].to_i,
       recursion_customer_7:  em.recursion['customer_id']['day_7'].to_i,
