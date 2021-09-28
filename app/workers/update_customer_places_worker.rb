3721ad5d30262c497d35ceecb3f232cd3171ad36
Toti Cinque <director@emoney.tech>
2021-09-28 11:42:18 +0200

    bugfix

--- app/workers/update_customer_places_worker.rb
+++ app/workers/update_customer_places_worker.rb
@@ -2,7 +2,7 @@ class UpdateCustomerPlacesWorker
   include Sidekiq::Worker
   include Sidekiq::Status::Worker
   # sidekiq_options queue: 'critical', retry: false, backtrace: true
-  sidekiq_options queue: 'default', retry: false, backtrace: true
+  sidekiq_options queue: 'massive', retry: false, backtrace: true
   
   # PARAMS
   # 
