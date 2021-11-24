5beed17d61afb28324803d3167597d0ca1b616e7
Toti Cinque <director@emoney.tech>
2021-11-24 11:09:48 +0100

    bugfix

--- app/controllers/data_analysis/movimenticonti_controller.rb
+++ app/controllers/data_analysis/movimenticonti_controller.rb
@@ -7,7 +7,7 @@ module DataAnalysis
         params[:filter] = { :daterange => "#{(Date.today - 1.month).strftime("%d/%m/%Y")} - #{Date.today.strftime("%d/%m/%Y")}"}
       end
       # @cached_total_pages = (RowCount.where(relname: 'movimenticonti').pluck(:reltuples).first / 30.0).ceil
-      movimenticonti = Movimentoconto.filter(filtering_params).only_customers.includes(:servizio, :anagrafica)
+      movimenticonti = Movimentoconto.filter(filtering_params).only_customers.includes(:servizio, :anagrafica, :evaluated_movement)
       #@count = 
       # @cached_total_pages = (movimenticonti.count / 30.0).ceil
       @movimenticonti = movimenticonti.order(movimentoconto_id: :desc).page(params[:page]).per(30).without_count
