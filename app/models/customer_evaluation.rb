9074c0e26d71130a3bba548b1851f1a09dccf17e
Toti Cinque <director@emoney.tech>
2021-11-25 11:00:36 +0100

    bugfix

--- app/models/customer_evaluation.rb
+++ app/models/customer_evaluation.rb
@@ -134,67 +134,6 @@ class CustomerEvaluation < CorePgRecord
     self.eval_days = hash_recalculate
   end
 
-=begin
-  def build_for_date(
-      day = nil,
-      evaluated_movements_for_day = nil,
-      max_base_risk = Configurable.max_base_risk.to_f,
-      min_base_risk = Configurable.min_base_risk.to_f,
-      tlf = Configurable.time_lapse_factor.to_f
-    )
-    day = Date.today unless day
-    evaluated_movements_for_day = evaluated_movements.with_all_for_day(day).as_json unless evaluated_movements_for_day
-    hash = {}
-    evaluated_movements_for_day.each do |evaluated_movement|
-      hash.merge!({
-        "#{evaluated_movement["movement_id"]}" => {
-          "day_7" => evaluated_movement["evaluated_factor7"],
-          "day_30" => evaluated_movement["evaluated_factor30"],
-        }
-      })
-    end
-    self.eval_days = {} if self.eval_days == "{}"
-    h2 = { movements: hash }
-    self.eval_days["#{day}"] = { movements: hash }
-    if day == Date.new(self.eval_year, self.eval_month, 1)
-      attention_factor = get_previuos_attention_factor_for_tuple(min_base_risk)
-    else
-      attention_factor = self.eval_days["#{day - 1.days}"]["details"]["attention_factor_decreased"]
-    end
-    hash.values.each do |v|
-      attention_factor["day_7"]  = attention_factor["day_7"].to_f  * v["day_7"].to_f
-      attention_factor["day_30"] = attention_factor["day_30"].to_f * v["day_30"].to_f
-    end
-    attention_factor["day_7"] = attention_factor["day_7"] >= max_base_risk ? max_base_risk : attention_factor["day_7"]
-    attention_factor["day_30"] = attention_factor["day_30"] >= max_base_risk ? max_base_risk : attention_factor["day_30"]
-    
-    attention_factor_decreased = {}
-    attention_factor_decreased["day_7"] = (attention_factor["day_7"] * tlf).to_f
-    attention_factor_decreased["day_7"] = attention_factor_decreased["day_7"] <= min_base_risk ? min_base_risk : attention_factor_decreased["day_7"]
-
-    attention_factor_decreased["day_30"] = (attention_factor["day_30"] * tlf).to_f
-    attention_factor_decreased["day_30"] = attention_factor_decreased["day_30"] <= min_base_risk ? min_base_risk : attention_factor_decreased["day_30"]
-    
-    hash2 = {
-      "attention_factor" => attention_factor,
-      "nr_movements" => evaluated_movements_for_day.count,
-      "attention_factor_decreased" => attention_factor_decreased
-    }
-    self.eval_days["#{day}"]["details"] = hash2    
-  end
-
-  def build_for_tuple(max_base_risk, min_base_risk, tlf, factor_for_amount, divisor_amount_for_factor, evaluated_movements)
-    date = Date.new( eval_year, eval_month, 1 )
-    date_end = date.end_of_month >= Date.today ? Date.today : date.end_of_month 
-    evaluated_days = {}
-    while (date <= date_end)
-      evaluated_movements_for_day = evaluated_movements.select{|h| h["day"]=="#{date}"}
-      self.build_for_date(date, evaluated_movements_for_day, max_base_risk, min_base_risk, tlf)
-      self.save
-      date = date.advance(days: 1)
-    end
-  end
-=end
   def decrease_factor(attention_factor, min_base_risk, tlf)
     hsh = {}
     hsh["day_7"] = (attention_factor["day_7"] * tlf).to_f
