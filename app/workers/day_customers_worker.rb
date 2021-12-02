a8e1eb7477ee1f7c6d22c254c201f7140bafe17e
Toti Cinque <director@emoney.tech>
2021-12-02 09:40:39 +0100

    bugfix

--- app/workers/day_customers_worker.rb
+++ app/workers/day_customers_worker.rb
@@ -8,7 +8,7 @@ class DayCustomersWorker
 
   def perform()
     # CustomerEvaluation.where(eval_year: Date.today.year, eval_month: Date.today.month).where.not('eval_days ?| array[:keys]', keys: ["#{Date.today - 1.day}"]).count
-    CustomerEvaluation.select(:anagrafica_id).where(eval_year: Date.today.year, eval_month: Date.today.month).where.not('eval_days ?| array[:keys]', keys: ["#{Date.today}"]).find_in_batches(batch_size: 500) do |evaluations|
+    CustomerEvaluation.select(:id, :anagrafica_id).where(eval_year: Date.today.year, eval_month: Date.today.month).where.not('eval_days ?| array[:keys]', keys: ["#{Date.today}"]).find_in_batches(batch_size: 500) do |evaluations|
       evaluations.each do |evaluation|
         DayCustomerWorker.perform_async(evaluation.anagrafica_id)
       end
