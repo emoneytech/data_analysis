0ed81224eedd96cec4b8ee9bc6a938a7ecae4680
Toti Cinque <director@emoney.tech>
2021-12-07 11:27:19 +0100

    bugfix

--- app/workers/recalculate_recursion_worker.rb
+++ app/workers/recalculate_recursion_worker.rb
@@ -2,7 +2,7 @@ class RecalculateRecursionWorker
   include Sidekiq::Worker
   include Sidekiq::Status::Worker
   # sidekiq_options queue: 'critical', retry: false, backtrace: true
-  sidekiq_options queue: 'serial', retry: true, backtrace: true
+  sidekiq_options queue: 'massive', retry: true, backtrace: true
   
   # PARAMS
   # evaluated_movement_id
--- app/workers/recalculate_recursions_worker.rb
+++ app/workers/recalculate_recursions_worker.rb
@@ -2,12 +2,12 @@ class RecalculateRecursionsWorker
   include Sidekiq::Worker
   include Sidekiq::Status::Worker
   # sidekiq_options queue: 'critical', retry: false, backtrace: true
-  sidekiq_options queue: 'serial', retry: true, backtrace: true
+  sidekiq_options queue: 'massive', retry: true, backtrace: true
   
   # PARAMS
 
   def perform()
-    EvaluatedMovement.select(:id).find_in_batches(batch_size: 500) do |evaluated_movements|
+    EvaluatedMovement.select(:id).find_in_batches(batch_size: 1000) do |evaluated_movements|
       evaluated_movements.each do |evaluated_movement|
         RecalculateRecursionWorker.perform_async(evaluated_movement.id)
       end
