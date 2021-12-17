fa53be7841cdde42d5dd850d07d5b35f7fe46dac
Toti Cinque <director@emoney.tech>
2021-12-17 12:38:38 +0100

    bugfix

--- app/workers/init_customer_evaluations_worker.rb
+++ app/workers/init_customer_evaluations_worker.rb
@@ -2,12 +2,12 @@ class InitCustomerEvaluationsWorker
   include Sidekiq::Worker
   include Sidekiq::Status::Worker
 
-  sidekiq_options queue: 'massive', retry: true, backtrace: true
+  sidekiq_options queue: 'critical', retry: true, backtrace: true
   
   # PARAMS
 
   def perform()
-    Anagrafica.alive.select(:IdUtente).find_in_batches(batch_size: 50) do |customers|
+    Anagrafica.alive.select(:IdUtente).find_in_batches(batch_size: 500) do |customers|
       customers.each do |customer|
         InitCustomerEvaluationWorker.perform_async(customer.IdUtente)
       end
