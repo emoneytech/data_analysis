23a0b0621048bf53d8c0d4677232b7a1c00272f9
Toti Cinque <director@emoney.tech>
2021-12-27 12:56:56 +0100

    bugfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "drkiq_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'data-analysis.emoney.tech:8443' }
+  config.action_mailer.default_url_options = { host: 'https://data-analysis.emoney.tech:8443' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
