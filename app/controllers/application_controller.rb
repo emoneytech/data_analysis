813ec6f1d1fbf2ae8713f031546e554eab0dd811
Toti Cinque <director@emoney.tech>
2022-01-07 14:14:24 +0100

    bugfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ class ApplicationController < ActionController::Base
   before_action :record_activity
   before_action :get_module_name
   add_flash_types :info, :error, :warning
+
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden }
+      format.html { redirect_to root_path, alert: exception.message }
+    end
+  end
+
+
 private
   def get_module_name
     my_class_name = self.class.name
