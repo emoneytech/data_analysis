24cc782ef13b452a2de0d909d7a4322c37ac5236
Emoney Tech ltd <80517292+emoneytech@users.noreply.github.com>
2022-01-19 10:32:22 +0100

    bugfix

--- app/controllers/customers/evaluated_movements_controller.rb
+++ app/controllers/customers/evaluated_movements_controller.rb
@@ -1,31 +1,81 @@
 module Customers
   class EvaluatedMovementsController < CustomersController
 
-    before_action :local_breadcrumb #, :init_risk
+    before_action :set_daterange, :local_breadcrumb #, :init_risk
 
     def index
-      @evaluated_movements = @anagrafica.evaluated_movements.order(movement_created_at: :desc).page(params[:page]).per(params[:per])
+      @evaluated_movements = EvaluatedMovement.filter(filtering_params).where(customer_id: @anagrafica.id)#.includes()
+      @evaluated_movements = @evaluated_movements.order(movement_created_at: :desc).page(params[:page]).per(params[:per])
     end
 
     def show
-      add_breadcrumb helpers.raw("#{helpers.fa_icon('search-dollar')} #{t(:show_resource, resource: EvaluatedMovement.model_name.human)}"), [:data_analysis, :evaluated_movement]
+      add_breadcrumb helpers.raw("#{helpers.fa_icon(EvaluatedMovement.icon)} #{t(:show_resource, resource: EvaluatedMovement.model_name.human)}"), [:data_analysis, :evaluated_movement]
       @evaluated_movement = @anagrafica.evaluated_movements.find(params[:id])
       @prev = @evaluated_movement.previous
       @next = @evaluated_movement.next
     end
 
+    def recursive
+      authorize! :read, EvaluatedMovement
+      add_breadcrumb helpers.raw("#{helpers.fa_icon(EvaluatedMovement.icon)} #{t(:show_resource, resource: EvaluatedMovement.model_name.human)}"), [:data_analysis, :evaluated_movement]
+      add_breadcrumb helpers.raw("#{t(:show_resource, resource: "Recursions")}"), [:recursion, :data_analysis, :evaluated_movement]
+      method_name = "recursive_for_#{params[:q]}"
+      @recursions = @evaluated_movement.send(method_name.to_sym, params[:days].to_i).includes(:triggerable).page(params[:page])
+      # render 'index'
+    end
+
     def for_day
-      @evaluated_movements = @anagrafica.evaluated_movements.with_all_for_day(params[:day].to_date).order(movement_created_at: :desc).page(params[:page]).per(params[:per])
+      @daterange = params[:filter] && params[:filter][:daterange] ? params[:filter][:daterange] : "#{params[:day].to_date.strftime("%d/%m/%Y")} - #{params[:day].to_date.strftime("%d/%m/%Y")}"
+      @evaluated_movements = @anagrafica.evaluated_movements.for_day(params[:day].to_date).order(recursion_all_7: :desc).page(params[:page]).per(params[:per])
+      render 'index'
     end
 
     def for_month
       month = DateTime.new(params[:year].to_i, params[:month].to_i, 1)
+      @daterange = params[:filter] && params[:filter][:daterange] ? params[:filter][:daterange] : "#{month.at_beginning_of_month.strftime("%d/%m/%Y")} - #{month.at_end_of_month.strftime("%d/%m/%Y")}"
       @evaluated_movements = @anagrafica.evaluated_movements.with_all_for_month(month).order(movement_created_at: :desc).page(params[:page]).per(params[:per])
+      render 'index'
     end
 
   private
+
+    def set_daterange
+      @daterange = params[:filter] && params[:filter][:daterange] ? params[:filter][:daterange] : "#{(Date.today - 1.year).strftime("%d/%m/%Y")} - #{Date.today.strftime("%d/%m/%Y")}"
+    end
+
+    def filtering_params
+      params[:filter] ? params[:filter].slice(
+        :beneficiary,
+        :beneficiary_card,
+        :beneficiary_iban,
+        :beneficiary_other,
+        :payer,
+        :payer_card,
+        :payer_iban,
+        :payer_other,
+        :customer_id,
+        :customer_full_name,
+        :daterange,
+        :destination_country,
+        :in_out,
+        :internal,
+        :min_recursion_all_7,
+        :min_recursion_all_30,
+        :min_recursion_customer_7,
+        :min_recursion_customer_30,
+        :movement_id,
+        :origin_country,
+        :payer,
+        :payer_iban,
+        :product_name,
+        :recursion_all_7,
+        :service_id,
+        :reason
+      ).permit! : {}
+    end
+
     def local_breadcrumb
-      add_breadcrumb helpers.raw("#{helpers.fa_icon('search-dollar')} #{EvaluatedMovement.model_name.human(count: 2)}"), [:customers, @anagrafica, :evaluated_movements]
+      add_breadcrumb helpers.raw("#{helpers.fa_icon(EvaluatedMovement.icon)} #{EvaluatedMovement.model_name.human(count: 2)}"), [:customers, @anagrafica, :evaluated_movements]
     end
 
   end
