ad6432be7e2782ce0dc30268d1dd4a908cf79d71
Emoney Tech ltd <80517292+emoneytech@users.noreply.github.com>
2022-01-31 10:17:05 +0100

    bugfix

--- app/workers/create_report_xlsx_worker.rb
+++ app/workers/create_report_xlsx_worker.rb
@@ -14,7 +14,9 @@ class CreateReportXlsxWorker
   def perform(report_id)
     report = Report.find(report_id) rescue nil
     return unless report
-    file_name = "#{Rails.root}/private/spreadsheets/#{report.name}.xlsx"
+    
+    file_name = "#{report.user_id}-#{report.name}.xlsx"
+    file_path = "#{Rails.root}/tmp/#{file_name}"
     the_klass = eval(report.opts["dataSource"])
     attributes = the_klass.export_attributes
     items = the_klass.filter(report.opts["filter"])
@@ -28,8 +30,8 @@ class CreateReportXlsxWorker
       end
     end
     stream = p.to_stream
-    File.open(file_name, 'w') { |f| f.write(stream.read) }
-    report.xls_file.attach(io: File.open(file_name), filename: file_name, content_type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    File.open(file_path, 'w') { |f| f.write(stream.read) }
+    report.xls_file.attach(io: File.open(file_path), filename: file_name, content_type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
     File.delete(file_name)
   end
 
