7eef60f042eecd2f78d090394602f1c7ec9cf79b
Emoney Tech ltd <80517292+emoneytech@users.noreply.github.com>
2022-02-11 11:55:28 +0100

    bugfix

--- app/controllers/data_analysis/evaluated_movements_controller.rb
+++ app/controllers/data_analysis/evaluated_movements_controller.rb
@@ -1,14 +1,28 @@
 module DataAnalysis
   class EvaluatedMovementsController < DataAnalysisController
-    add_breadcrumb helpers.raw("#{helpers.fa_icon(EvaluatedMovement.icon)} #{EvaluatedMovement.model_name.human(count: 2)}"), [:data_analysis, :evaluated_movements]
+    add_breadcrumb helpers.raw(
+                     "#{helpers.fa_icon(EvaluatedMovement.icon)} #{EvaluatedMovement.model_name.human(count: 2)}",
+                   ),
+                   %i[data_analysis evaluated_movements]
+
     # before_action :set_daterange, only: [:index, :recursive]
 
     def index
       unless params[:filter] && params[:filter][:daterange]
-        params[:filter] = { :daterange => "#{(Date.today - 1.month).strftime("%d/%m/%Y")} - #{Date.today.strftime("%d/%m/%Y")}"}
+        params[:filter] = {
+          daterange:
+            "#{(Date.today - 1.month).strftime('%d/%m/%Y')} - #{Date.today.strftime('%d/%m/%Y')}",
+        }
       end
-      evaluated_movements = EvaluatedMovement.filter(filtering_params).includes(:customer, :triggerable)
-      @evaluated_movements = evaluated_movements.order(movement_created_at: :desc).page(params[:page]).per(params[:per])
+      evaluated_movements =
+        EvaluatedMovement
+          .filter(filtering_params)
+          .includes(:customer, :triggerable)
+      @evaluated_movements =
+        evaluated_movements
+          .order(movement_created_at: :desc)
+          .page(params[:page])
+          .per(params[:per])
       respond_to do |format|
         format.html
         format.json
@@ -18,75 +32,116 @@ module DataAnalysis
     def data_table
       respond_to do |format|
         format.html
-        format.json { 
-          render json: EvaluatedMovementDatatable.new(params)
-        }
+        format.json { render json: EvaluatedMovementDatatable.new(params) }
       end
     end
 
     def show
-      add_breadcrumb helpers.raw("#{helpers.fa_icon(EvaluatedMovement.icon)} #{t(:show_resource, resource: EvaluatedMovement.model_name.human)}"), [:data_analysis, :evaluated_movement]
+      add_breadcrumb helpers.raw(
+                       "#{helpers.fa_icon(EvaluatedMovement.icon)} #{t(:show_resource, resource: EvaluatedMovement.model_name.human)}",
+                     ),
+                     %i[data_analysis evaluated_movement]
       @prev = @evaluated_movement.all_previous
       @next = @evaluated_movement.all_next
     end
 
     def recursive
       authorize! :read, EvaluatedMovement
-      add_breadcrumb helpers.raw("#{helpers.fa_icon(EvaluatedMovement.icon)} #{t(:show_resource, resource: EvaluatedMovement.model_name.human)}"), [:data_analysis, :evaluated_movement]
-      add_breadcrumb helpers.raw("#{t(:show_resource, resource: "Recursions")}"), [:recursion, :data_analysis, :evaluated_movement]
+      add_breadcrumb helpers.raw(
+                       "#{helpers.fa_icon(EvaluatedMovement.icon)} #{t(:show_resource, resource: EvaluatedMovement.model_name.human)}",
+                     ),
+                     %i[data_analysis evaluated_movement]
+      add_breadcrumb helpers.raw(
+                       "#{t(:show_resource, resource: 'Recursions')}",
+                     ),
+                     %i[recursion data_analysis evaluated_movement]
       method_name = "recursive_for_#{params[:q]}"
-      @recursions = @evaluated_movement.send(method_name.to_sym, params[:days].to_i).includes(:triggerable).page(params[:page])
+      @recursions =
+        @evaluated_movement
+          .send(method_name.to_sym, params[:days].to_i)
+          .includes(:triggerable)
+          .page(params[:page])
       # render 'index'
     end
 
     def for_day
-      @daterange = params[:filter] && params[:filter][:daterange] ? params[:filter][:daterange] : "#{params[:day].to_date.strftime("%d/%m/%Y")} - #{params[:day].to_date.strftime("%d/%m/%Y")}"
-      @evaluated_movements = @evaluated_movements.for_day(params[:day].to_date).order(recursion_all_7: :desc).page(params[:page]).per(params[:per])
+      @daterange =
+        if params[:filter] && params[:filter][:daterange]
+          params[:filter][:daterange]
+        else
+          "#{params[:day].to_date.strftime('%d/%m/%Y')} - #{params[:day].to_date.strftime('%d/%m/%Y')}"
+        end
+      @evaluated_movements =
+        @evaluated_movements
+          .for_day(params[:day].to_date)
+          .order(recursion_all_7: :desc)
+          .page(params[:page])
+          .per(params[:per])
       render 'index'
     end
 
     def for_month
       month = DateTime.new(params[:year].to_i, params[:month].to_i, 1)
-      @daterange = params[:filter] && params[:filter][:daterange] ? params[:filter][:daterange] : "#{month.at_beginning_of_month.strftime("%d/%m/%Y")} - #{month.at_end_of_month.strftime("%d/%m/%Y")}"
-      @evaluated_movements = @evaluated_movements.with_all_for_month(month).order(movement_created_at: :desc).page(params[:page]).per(params[:per])
+      @daterange =
+        if params[:filter] && params[:filter][:daterange]
+          params[:filter][:daterange]
+        else
+          "#{month.at_beginning_of_month.strftime('%d/%m/%Y')} - #{month.at_end_of_month.strftime('%d/%m/%Y')}"
+        end
+      @evaluated_movements =
+        @evaluated_movements
+          .with_all_for_month(month)
+          .order(movement_created_at: :desc)
+          .page(params[:page])
+          .per(params[:per])
       render 'index'
     end
 
-  private
+    private
+
     def set_daterange
-      @daterange = params[:filter] && params[:filter][:daterange] ? params[:filter][:daterange] : "#{(Date.today - 1.year).strftime("%d/%m/%Y")} - #{Date.today.strftime("%d/%m/%Y")}"
+      @daterange =
+        if params[:filter] && params[:filter][:daterange]
+          params[:filter][:daterange]
+        else
+          "#{(Date.today - 1.year).strftime('%d/%m/%Y')} - #{Date.today.strftime('%d/%m/%Y')}"
+        end
     end
 
     def filtering_params
-      params[:filter] ? params[:filter].slice(
-        :beneficiary,
-        :beneficiary_card,
-        :beneficiary_iban,
-        :beneficiary_other,
-        :payer,
-        :payer_card,
-        :payer_iban,
-        :payer_other,
-        :customer_id,
-        :customer_full_name,
-        :daterange,
-        :destination_country,
-        :in_out,
-        :internal,
-        :min_recursion_all_7,
-        :min_recursion_all_30,
-        :min_recursion_customer_7,
-        :min_recursion_customer_30,
-        :movement_id,
-        :origin_country,
-        :payer,
-        :payer_iban,
-        :product_name,
-        :recursion_all_7,
-        :service_id,
-        :reason
-      ).permit! : {}
+      if params[:filter]
+        params[:filter].slice(
+          :beneficiary,
+          :beneficiary_card,
+          :beneficiary_iban,
+          :beneficiary_other,
+          :payer,
+          :payer_card,
+          :payer_iban,
+          :payer_other,
+          :customer_id,
+          :customer_full_name,
+          :daterange,
+          :destination_country,
+          :in_out,
+          :internal,
+          :min_recursion_all_7,
+          :min_recursion_all_30,
+          :min_recursion_customer_7,
+          :min_recursion_customer_30,
+          :movement_id,
+          :origin_country,
+          :payer,
+          :payer_iban,
+          :product_id,
+          :product_name,
+          :recursion_all_7,
+          :service_id,
+          :reason,
+        ).permit!
+      else
+        {}
+      end
     end
-    
   end
 end
--- app/models/concerns/evaluated_movement_filters.rb
+++ app/models/concerns/evaluated_movement_filters.rb
@@ -3,101 +3,185 @@ module EvaluatedMovementFilters
 
   included do
     # filters
-    scope :filter_by_customer_id, -> (customer_id) { where(customer_id: customer_id)}
-    scope :filter_by_service_id, -> (service_id) { where(triggerable_type: 'Servizio', triggerable_id: service_id)}
-    scope :filter_by_movement_id, -> (movement_id) { where(movement_id: movement_id)}
+    scope :filter_by_customer_id,
+          ->(customer_id) { where(customer_id: customer_id) }
+    scope :filter_by_service_id,
+          ->(service_id) {
+            where(triggerable_type: 'Servizio', triggerable_id: service_id)
+          }
+    scope :filter_by_movement_id,
+          ->(movement_id) { where(movement_id: movement_id) }
 
-    scope :filter_by_customer_full_name, -> (name) { where(customer_id: Anagrafica.filter_by_full_name(name).pluck(:IdUtente))}
+    scope :filter_by_customer_full_name,
+          ->(name) {
+            where(
+              customer_id:
+                Anagrafica.filter_by_full_name(name).pluck(:IdUtente),
+            )
+          }
 
-    scope :filter_by_beneficiary, -> (name) { where("beneficiary ilike ?", "%#{name}%")}
-    scope :filter_by_beneficiary_card, -> (beneficiary_card) { where("beneficiary_card ilike ?", "%#{beneficiary_card}%")}
-    scope :filter_by_beneficiary_iban, -> (beneficiary_iban) { where("beneficiary_iban ilike ?", "%#{beneficiary_iban}%")}
-    scope :filter_by_beneficiary_other, -> (beneficiary_other) { where("beneficiary_other ilike ?", "%#{beneficiary_other}%")}
+    scope :filter_by_beneficiary,
+          ->(name) { where('beneficiary ilike ?', "%#{name}%") }
+    scope :filter_by_beneficiary_card,
+          ->(beneficiary_card) {
+            where('beneficiary_card ilike ?', "%#{beneficiary_card}%")
+          }
+    scope :filter_by_beneficiary_iban,
+          ->(beneficiary_iban) {
+            where('beneficiary_iban ilike ?', "%#{beneficiary_iban}%")
+          }
+    scope :filter_by_beneficiary_other,
+          ->(beneficiary_other) {
+            where('beneficiary_other ilike ?', "%#{beneficiary_other}%")
+          }
 
-    scope :filter_by_payer, -> (name) { where("payer ilike ?", "%#{name}%")}
-    scope :filter_by_payer_card, -> (payer_card) { where("payer_card ilike ?", "%#{payer_card}%")}
-    scope :filter_by_payer_iban, -> (payer_iban) { where("payer_iban ilike ?", "%#{payer_iban}%")}
-    scope :filter_by_payer_other, -> (payer_other) { where("payer_other ilike ?", "%#{payer_other}%")}
+    scope :filter_by_payer, ->(name) { where('payer ilike ?', "%#{name}%") }
+    scope :filter_by_payer_card,
+          ->(payer_card) { where('payer_card ilike ?', "%#{payer_card}%") }
+    scope :filter_by_payer_iban,
+          ->(payer_iban) { where('payer_iban ilike ?', "%#{payer_iban}%") }
+    scope :filter_by_payer_other,
+          ->(payer_other) { where('payer_other ilike ?', "%#{payer_other}%") }
 
-    scope :filter_by_product_name, -> (product_name) { where("product_name ilike ?", "%#{product_name}%")}
+    scope :filter_by_product_name,
+          ->(product_name) {
+            where('product_name ilike ?', "%#{product_name}%")
+          }
+    scope :filter_by_product_id,
+          ->(product_id) { where('product_id = ?', product_id) }
 
-    scope :filter_by_daterange, ->(daterange) {
-      where("movement_created_at BETWEEN '#{daterange.split(' - ')[0].to_date.beginning_of_day}' 
-        AND '#{daterange.split(' - ')[1].to_date.end_of_day}'")
-    }
+    scope :filter_by_daterange,
+          ->(daterange) {
+            where(
+              "movement_created_at BETWEEN '#{daterange.split(' - ')[0].to_date.beginning_of_day}' 
+        AND '#{daterange.split(' - ')[1].to_date.end_of_day}'",
+            )
+          }
 
-    scope :with_all_for_day, -> (day) {
-      where(
-        "movement_created_at::date = ?", day
-      )
-    }
+    scope :with_all_for_day,
+          ->(day) { where('movement_created_at::date = ?', day) }
 
-    scope :with_all_for_month, -> (month) {
-      where(
-        "EXTRACT(MONTH FROM movement_created_at) = ?", month
-      )
-    }
+    scope :with_all_for_month,
+          ->(month) {
+            where('EXTRACT(MONTH FROM movement_created_at) = ?', month)
+          }
 
-    scope :with_all_for_year, -> (year) {
-      where(
-        "EXTRACT(YEAR FROM movement_created_at) = ?", year
-      )
-    }
+    scope :with_all_for_year,
+          ->(year) { where('EXTRACT(YEAR FROM movement_created_at) = ?', year) }
 
-    scope :filter_by_origin_country, -> (country) { where("origin_country ilike ?", "#{country}")}
-    scope :filter_by_destination_country, -> (country) { where("destination_country ilike ?", "#{country}")}
+    scope :filter_by_origin_country,
+          ->(country) { where('origin_country ilike ?', "#{country}") }
+    scope :filter_by_destination_country,
+          ->(country) { where('destination_country ilike ?', "#{country}") }
 
-    scope :filter_by_amount,     -> (amount_cents) { where("amount_cents  = ?", amount_cents)}
-    scope :filter_by_min_amount, -> (amount_cents) { where("amount_cents >= ?", amount_cents)}
-    scope :filter_by_max_amount, -> (amount_cents) { where("amount_cents <= ?", amount_cents)}
+    scope :filter_by_amount,
+          ->(amount_cents) { where('amount_cents  = ?', amount_cents) }
+    scope :filter_by_min_amount,
+          ->(amount_cents) { where('amount_cents >= ?', amount_cents) }
+    scope :filter_by_max_amount,
+          ->(amount_cents) { where('amount_cents <= ?', amount_cents) }
 
-    scope :filter_by_recursion_all_7,       -> (recursion_all_7) { where("recursion_all_7  = ?", recursion_all_7)}
-    scope :filter_by_min_recursion_all_7,   -> (recursion_all_7) { where("recursion_all_7 >= ?", recursion_all_7)}
-    scope :filter_by_max_recursion_all_7,   -> (recursion_all_7) { where("recursion_all_7 <= ?", recursion_all_7)}
+    scope :filter_by_recursion_all_7,
+          ->(recursion_all_7) { where('recursion_all_7  = ?', recursion_all_7) }
+    scope :filter_by_min_recursion_all_7,
+          ->(recursion_all_7) { where('recursion_all_7 >= ?', recursion_all_7) }
+    scope :filter_by_max_recursion_all_7,
+          ->(recursion_all_7) { where('recursion_all_7 <= ?', recursion_all_7) }
 
-    scope :filter_by_recursion_all_30,      -> (recursion_all_30) { where("recursion_all_30  = ?", recursion_all_30)}
-    scope :filter_by_min_recursion_all_30,  -> (recursion_all_30) { where("recursion_all_30 >= ?", recursion_all_30)}
-    scope :filter_by_max_recursion_all_30,  -> (recursion_all_30) { where("recursion_all_30 <= ?", recursion_all_30)}
+    scope :filter_by_recursion_all_30,
+          ->(recursion_all_30) {
+            where('recursion_all_30  = ?', recursion_all_30)
+          }
+    scope :filter_by_min_recursion_all_30,
+          ->(recursion_all_30) {
+            where('recursion_all_30 >= ?', recursion_all_30)
+          }
+    scope :filter_by_max_recursion_all_30,
+          ->(recursion_all_30) {
+            where('recursion_all_30 <= ?', recursion_all_30)
+          }
 
-    scope :filter_by_recursion_customer_7,     -> (recursion_customer_7) { where("recursion_customer_7  = ?", recursion_customer_7)}
-    scope :filter_by_min_recursion_customer_7, -> (recursion_customer_7) { where("recursion_customer_7 >= ?", recursion_customer_7)}
-    scope :filter_by_max_recursion_customer_7, -> (recursion_customer_7) { where("recursion_customer_7 <= ?", recursion_customer_7)}
+    scope :filter_by_recursion_customer_7,
+          ->(recursion_customer_7) {
+            where('recursion_customer_7  = ?', recursion_customer_7)
+          }
+    scope :filter_by_min_recursion_customer_7,
+          ->(recursion_customer_7) {
+            where('recursion_customer_7 >= ?', recursion_customer_7)
+          }
+    scope :filter_by_max_recursion_customer_7,
+          ->(recursion_customer_7) {
+            where('recursion_customer_7 <= ?', recursion_customer_7)
+          }
 
-    scope :filter_by_recursion_customer_30,     -> (recursion_customer_30) { where("recursion_customer_30  = ?", recursion_customer_30)}
-    scope :filter_by_min_recursion_customer_30, -> (recursion_customer_30) { where("recursion_customer_30 >= ?", recursion_customer_30)}
-    scope :filter_by_max_recursion_customer_30, -> (recursion_customer_30) { where("recursion_customer_30 <= ?", recursion_customer_30)}
+    scope :filter_by_recursion_customer_30,
+          ->(recursion_customer_30) {
+            where('recursion_customer_30  = ?', recursion_customer_30)
+          }
+    scope :filter_by_min_recursion_customer_30,
+          ->(recursion_customer_30) {
+            where('recursion_customer_30 >= ?', recursion_customer_30)
+          }
+    scope :filter_by_max_recursion_customer_30,
+          ->(recursion_customer_30) {
+            where('recursion_customer_30 <= ?', recursion_customer_30)
+          }
 
-    scope :filter_by_internal, -> (internal) { where(internal: internal)}
-    scope :filter_by_in_out, -> (value) { where(in_out: value)}
+    scope :filter_by_internal, ->(internal) { where(internal: internal) }
+    scope :filter_by_in_out, ->(value) { where(in_out: value) }
 
-    scope :filter_by_reason, -> (reason) { where("reason iLIKE ?", "%#{reason}%")}
+    scope :filter_by_reason,
+          ->(reason) { where('reason iLIKE ?', "%#{reason}%") }
 
-    scope :remove_max, -> (size) { where.not(id: EvaluatedMovement.order(amount_cents: :desc).limit(size).pluck(:id)) }
-    scope :remove_min, -> (size) { where.not(id: EvaluatedMovement.order(amount_cents: :asc).limit(size).pluck(:id)) }
+    scope :remove_max,
+          ->(size) {
+            where.not(
+              id:
+                EvaluatedMovement
+                  .order(amount_cents: :desc)
+                  .limit(size)
+                  .pluck(:id),
+            )
+          }
+    scope :remove_min,
+          ->(size) {
+            where.not(
+              id:
+                EvaluatedMovement
+                  .order(amount_cents: :asc)
+                  .limit(size)
+                  .pluck(:id),
+            )
+          }
 
-    scope :for_medium, -> (size) { remove_max(size).remove_min(size) }
+    scope :for_medium, ->(size) { remove_max(size).remove_min(size) }
+
     # PostGIS SPATIAL QUERIES
     # for finding place X distance from a particular point (i.e. radius)
-    scope :geocoded, -> { where.not( destination_lonlat: nil) }
+    scope :geocoded, -> { where.not(destination_lonlat: nil) }
 
-    scope :within, -> (lon, lat, meter) {
-      where(%{
+    scope :within,
+          ->(lon, lat, meter) {
+            where(
+              "
       ST_Distance(destination_lonlat, 'POINT(%f %f)') < %d
-      } % [lon, lat, meter])
-    }
+      " % [lon, lat, meter],
+            )
+          }
 
     # for finding trees within a certain bounding box
-    scope :bbox, -> (sw_lon, sw_lat, ne_lon, ne_lat) {
-      factory = RGeo::Geographic.spherical_factory(srid: 4326, geo_type: 'point')
-      sw = factory.point(sw_lon, sw_lat)
-      nw = factory.point(sw_lon, ne_lat)
-      ne = factory.point(ne_lon, ne_lat)
-      se = factory.point(ne_lon, sw_lat)
+    scope :bbox,
+          ->(sw_lon, sw_lat, ne_lon, ne_lat) {
+            factory =
+              RGeo::Geographic.spherical_factory(srid: 4326, geo_type: 'point')
+            sw = factory.point(sw_lon, sw_lat)
+            nw = factory.point(sw_lon, ne_lat)
+            ne = factory.point(ne_lon, ne_lat)
+            se = factory.point(ne_lon, sw_lat)
 
-      ring = factory.linear_ring([sw, nw, ne, se])
-      bbox = factory.polygon(ring)
-      where('ST_Intersects(destination_lonlat, :bbox)', bbox: bbox)
-    }
+            ring = factory.linear_ring([sw, nw, ne, se])
+            bbox = factory.polygon(ring)
+            where('ST_Intersects(destination_lonlat, :bbox)', bbox: bbox)
+          }
   end
-
 end
