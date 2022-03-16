3077252c200b0a3d9c09e9ecb0b0303b99cc5720
Emoney Tech ltd <80517292+emoneytech@users.noreply.github.com>
2022-03-16 14:32:08 +0100

    bugfix

--- app/workers/import_csv_sanction_list_worker.rb
+++ app/workers/import_csv_sanction_list_worker.rb
@@ -11,6 +11,15 @@ class ImportCsvSanctionListWorker
   def perform(sanction_list_id)
     sanction_list = SanctionList.find(sanction_list_id) rescue nil
     return unless sanction_list
-    sanction_list.csv_smarter
+    sanction_list.main_csv.open do |file|
+      headers = CSV.open(file, encoding: 'bom|utf-8', &:readline)
+      ary = headers[0].split(";")
+      opts = {}
+      ary.each do |h|
+        opts[h.downcase.to_sym] = h.underscore.to_sym
+      end
+      n = SmarterCSV.process(file.path, {chunk_size: 2000, key_mapping: opts, file_encoding: 'bom|utf-8', col_sep: ";"}) do |chunk|
+        sanction_list.sanction_list_items.create( chunk )
+      end
   end
 end
